98938881c567432eca753aa6eff4b8dc7e3a80cf
Damir <ahmedjanovdamir65@gmail.com>
2023-05-28 11:40:04 +0500

    added info about homework

---
+++ seminar3/task3.py
@@ -0,0 +1,21 @@
+# Напишите программу для печати всех уникальных 
+# значений в словаре. 
+# Input: [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, 
+# {"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII 
+# ":" S007 "}] 
+
+input_list = [
+    {"V": "S001"},
+    {"V": "S002"},
+    {"VI": "S001"},
+    {"VI": "S005"},
+    {"VII": "S005"},
+    {"V": "S009"},
+    {"VIII": "S007"}
+]
+
+unique_values = set()
+for dictionary in input_list:
+    unique_values.update(set(dictionary.values()))
+
+print(unique_values)
