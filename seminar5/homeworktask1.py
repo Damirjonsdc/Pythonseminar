1eaa0dd36b7053354a29dee9c781b6b8e0532fed
Damir <ahmedjanovdamir65@gmail.com>
2023-06-04 11:45:24 +0500

    added info about homework

---
+++ seminar5/homeworktask1.py
@@ -0,0 +1,16 @@
+#  Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии
+def power(A, B):
+    if B == 0:
+        return 1
+    elif B % 2 == 0:
+        return power(A, B//2) * power(A, B//2)
+    else:
+        return A * power(A, B//2) * power(A, B//2)
+
+A = int(input("Введите число A: "))
+B = int(input("Введите степень B: "))
+
+print(power(A, B))
+
+
+
